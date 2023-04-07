92cf58aae8ab2244f2ee5af3575c39a70f27710d
RTKwok <rtkkwok@hotmail.co.uk>
2023-04-07 15:26:45 +0100

    Initial Commit

---
+++ Data/rossCV_data.R
@@ -0,0 +1,134 @@
+
+# Employment --------------------------------------------------------------
+
+work <- tribble(
+  ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
+  "Sales Assistant", 
+  "The Works", 
+  NA, 2016, NA, NA, 
+  "Edinburgh, Scotland", 
+  "Position focussed on customer service and relations.",
+  
+  "Volunteer", 
+  "Chengdu Panda Breeding Research Base", 
+  NA, 2015, NA, NA, 
+  "Chengdu, China", 
+  "Husbandry and care for pandas as part of a conservation project.",
+  
+  "Volunteer", 
+  "National Trust", 
+  NA, 2022, NA, NA, 
+  "Edinburgh, Scotland", 
+  "Assisted in the construction of a `roundhouse`.",
+  
+  "Volunteer", 
+  "Oxfam Record Store", 
+  NA, 2012, NA, 2016, 
+  "Edinburgh, Scotland", 
+  "Sales assistant."
+)
+
+# Education ---------------------------------------------------------------
+
+edu <- tribble(
+  ~degree, ~startYear, ~endYear, ~inst, ~title,
+  "PhD in Forensic Science: Evaluating the Viability of Obtaining DNA Profiles from DNA Encapsulated Within the Layers of Counterfeit Banknotes", 
+  2019, 2022, 
+  "Staffordshire University", 
+  "Chapter 1: Literature Review.",
+  
+  "PhD in Forensic Science: Evaluating the Viability of Obtaining DNA Profiles from DNA Encapsulated Within the Layers of Counterfeit Banknotes", 
+  2019, 2022, 
+  "Staffordshire University", 
+  "Chapter 2: Evaluation of DNA in Counterfeit Banknotes.",
+  
+  "PhD in Forensic Science: Evaluating the Viability of Obtaining DNA Profiles from DNA Encapsulated Within the Layers of Counterfeit Banknotes", 
+  2019, 2022, 
+  "Staffordshire University", 
+  "Chapter 3: Simulated Procedural Study to Evaluate the Presence of DNA in Composite Counterfeit Banknotes.",
+  
+  "PhD in Forensic Science: Evaluating the Viability of Obtaining DNA Profiles from DNA Encapsulated Within the Layers of Counterfeit Banknotes", 
+  2019, 2022, 
+  "Staffordshire University", 
+  "Chapter 4: Genetic Evaluation of Counterfeit Banknotes.",
+  
+  "PhD in Forensic Science: Evaluating the Viability of Obtaining DNA Profiles from DNA Encapsulated Within the Layers of Counterfeit Banknotes", 
+  2019, 2022, 
+  "Staffordshire University", 
+  "Chapter 5: Conclusions and Future Considerations.",
+  
+  "Msc First Class in Forensic Science", 
+  2017, 2018, 
+  "Staffordshire University", 
+  NA,
+  
+  "Msc in Genetics", 
+  2016, 2017, 
+  "University of Aberdeen", 
+  NA,
+  
+  "BSc (Hons) Upper Second Class in Zoology", 
+  2012, 2016, 
+  "University of Aberdeen", 
+  NA
+)
+
+# Awards ------------------------------------------------------------------
+
+conf <- tribble(
+  ~area, ~accomplishment, ~year, ~where, #~detail,
+  #"Award", 
+  "Guest speaker",
+  "Title: An Evaluation of Two Adhesive Media for the Recovery of DNA from Latent Finger-marks: A Preliminary Study",
+  2022, 
+  "The Centre for Crime, Justice and Security, University of Staffordshire", 
+  
+  #"Award", 
+  "Guest speaker", 
+  "Title: Evaluating the Viability of Obtaining DNA Profiles from DNA Encapsulated Within the Layers of Counterfeit Banknotes",
+  2022, 
+  "The Centre for Crime, Justice and Security, University of Staffordshire", 
+  
+  #"Funding", 
+  "Presenter at The 28th International Congress of the International Society of Forensic Genetics", 
+  "Title: XXX",
+  2019, 
+  "Prague"
+)
+
+# Publications ------------------------------------------------------------
+
+works <- tribble(
+  ~author, ~title, ~year, ~where, ~doi,
+  "Evaluating the viability of obtaining DNA profiles from DNA encapsulated between the layers of composite counterfeit banknotes.", 
+  "Kwok, R., Kenny, D. and Williams, G.A.", 
+  "2019", 
+  "Forensics Science International Genetics International: Genetics Supplement Series, 7, pp. 438-440", 
+  "https://doi.org/10.1016/j.fsigss.2019.10.043",
+  
+  "An evaluation of two adhesive media for the recovery of DNA from latent fingermarks: A preliminary study.", 
+  "Kwok, R., Parsons, R., Fieldhouse, S. and Walton-Williams, L.", 
+  "2023", 
+  "Forensic Science International, [online] 344(4), 111574", 
+  "https://doi.org/10.1016/j.forsciint.2023.111574"
+)
+
+# Participation -------------------------------------------------------------
+
+memb <- tribble(
+  ~group,
+  "Chartered Society of Forensic Science",
+  "International Society of Forensic Genetics"
+)
+
+# Skills ------------------------------------------------------------------
+
+skills <- tribble(
+  ~area, ~skills,
+  "Computer software", "Word, Excel, and related Microsoft software",
+  "Statistical software", "Minitab, SPSS, and (some) R",
+  "Genomic software", "MrBayes, DNAsp, FinchTV, CLC, Figtree, Arlequin, ClustalX, Proseq, BLAST, Funcassociate, MultiArray Viewer, GeneMapper",
+  "Laboratory skills", "Polymerase chain reaction techniques (qPCR), DNA/RNA extraction (phenol chloroform, chelex resin, silica spin columns and magnetic bead extractions), gel electrophoresis, agarose gel digestion, capillary electrophoresis, PCR thermocyclers (both for standard PCR and qPCR), fume hoods, gel electrophoresis equipment, heat blocks, qubit fluorometer and nanodrops",
+  "Forensic skills", "GRIM, MSP, PLM, TLC, fingerprint analysis, DNA profiling, blood spatter and presumptive and confirmative body fluid testing",
+  "Langauges", "English (Fluent), Cantonese (Fluent)"
+)
